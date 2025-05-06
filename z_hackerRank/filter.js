e9525579f443896186237e2c731f67389547781d
Marcela Ananias <marcelaananias154@gmail.com>
2025-05-06 16:57:45 -0300

    feat: implementar função para uso do filter

---
+++ z_hackerRank/filter.js
@@ -0,0 +1,43 @@
+
+//Calcule quantas vezes o maior número aparece no array
+
+candles = [4, 4, 1, 3]
+
+function birthdayCakeCandles(candles) {
+    const numMax = Math.max(...candles) //Usando 'Math.max' que pega o maior valor do array
+    
+    const biggerNum = candles.filter((candle) => { 
+        return candle === numMax //filtra os que correspondem com numMax
+    })
+    
+    return biggerNum.length; //Length retorna em quantidade
+}
+
+console.log(birthdayCakeCandles(candles))
+
+//const biggerNum = candles.filter(candle => candle === numMax); poderia ser assim também, onde 
+//return é automatico.
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Dado um array de números representando as notas de uma turma, conte quantos alunos tiraram acima da média da turma.
+
+notas = [70, 80, 90, 100]; //
+
+function media(notas){
+    const somaNotas = notas.reduce((acumulater, valorAtual) => { //Calculando total das notas
+        return acumulater + valorAtual
+    }, 0)
+
+    const mediaNotas = somaNotas / notas.length; //Calcando a média = 85
+
+    const filterMedia = notas.filter((nota) => nota >= mediaNotas) //Filtrando
+
+    return filterMedia;
+}
+
+console.log(media(notas))
+
--- z_hackerRank/repeat_escadaria.js
+++ z_hackerRank/repeat_escadaria.js
@@ -15,3 +15,44 @@ function staircase(n) {
 }
 
 staircase(n);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//Desafio: Escada Invertida
+//Crie uma função chamada reverseStaircase(n) que imprime uma escada invertida, com n degraus, usando o caractere #.
+
+
+const x = 4 
+
+function reverseStaircase(x) {
+    for (let r = 0; r < x; r++){
+
+        const space = '#'.repeat(x - r)
+        const step = ' '.repeat(r)
+
+        console.log(space + step)
+    }
+}
+
+reverseStaircase(x);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//Desafio: Pirâmide Centralizada
+//Faça uma função que desenhe uma pirâmide centralizada com n degraus (ou seja, linhas)
+
+
+const y = 4
+
+function pyramid(y){
+
+    for (let p = 1; p <= y; p++){
+
+        const ladoA = ' '.repeat(y - p)
+        const ladoB = '#'.repeat(2 * p - 1)
+
+        console.log(ladoA + ladoB)
+
+    }
+}
+
+pyramid(y)
